960f844f74fa1a153aca574b59804647818a521c
OptimistGD <optimist.professionel@gmail.com>
2025-12-17 10:00:49 +0100

    Commit pour Jules

---
+++ Assets/Script/ClickableButton.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ClickableButton : MonoBehaviour
+{
+    public void OnClicked()
+    {
+        
+    }
+}
---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject cylinderPrefab;
+    public GameObject buttonSpawner;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

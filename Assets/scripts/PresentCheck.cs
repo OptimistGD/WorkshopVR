c2e355d1d23546231d64c5afc6c99549003e95d1
kala <mignereleo@gmail.com>
2025-12-18 09:31:47 +0100

    prefab present final

--- Assets/scripts/PresentCheck.cs
+++ Assets/scripts/PresentCheck.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 public class PresentCheck : MonoBehaviour
 {
     [SerializeField] private GameObject lidPresent; 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log(other.gameObject.name);
-        
-        if (other.CompareTag("Toys"))
-            ClosePresent();
-    }
 
-    private void ClosePresent()
+    public void ClosePresent()
     {
         lidPresent.SetActive(true);
     }

4d248849e316a89a61c7f3a0207f161059ee31f3
OptimistGD <optimist.professionel@gmail.com>
2025-12-19 10:13:35 +0100

    Blocking presque fini + cadeaux fins

--- Assets/Script/OpenHatch.cs
+++ Assets/Script/OpenHatch.cs
@@ -1,44 +1,72 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class OpenHatch : MonoBehaviour
 {
-    private GameObject Hatch;
-    private GameObject HatchOpen;
-    private GameObject HatchClose;
+    private Vector3 closedPositionLeft;
+    private Vector3 closedPositionRight;
+    
+    [SerializeField] private GameObject hatchLeft;
+    [SerializeField] private GameObject hatchRight;
+    
+    [SerializeField] private Component leftOpen;
+    [SerializeField] private Component rightOpen;
 
     public bool hatchIsOpen = false;
-    public int moveSpeed = 3;
 
-    public GameObject button;
-
-    public void Start()
-    {
-        HatchClose = Instantiate(Hatch, Hatch.transform.position, Hatch.transform.rotation);
-        HatchClose.SetActive(false);
-        HatchOpen.SetActive(false);
-    }
-
-    public void Update()
-    {
-        var target = hatchIsOpen ? HatchOpen : HatchClose;
-        Hatch.transform.position = Vector3.MoveTowards(Hatch.transform.position, target.transform.position, moveSpeed * Time.deltaTime);
-    }
     
-    void OnTriggerEnter(Collider present)
-    {
-        hatchIsOpen = true;
-    }
     
-    void OnTriggerExit(Collider cube)
+    private void Awake()
     {
-        hatchIsOpen = false;
-        //ici sera le code du destroy des gifts
+        closedPositionLeft = hatchLeft.transform.localPosition;
+        closedPositionRight = hatchRight.transform.localPosition;
     }
 
-    public static void Open()
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Gift"))
+        {
+            StartCoroutine(hatchOpen());
+        }
+    }
+
+
+    public IEnumerator hatchOpen(Collider other) 
+    {
+        float currentMovementTime = 0f;
+        float totalMovementTime = 1f;
+
+        Vector3 openPositionL = leftOpen.transform.position;
+        Vector3 openPositionR = rightOpen.transform.position;
         
+        while (Vector3.Distance(hatchLeft.transform.localPosition, openPositionL) > 0) 
+        {
+            currentMovementTime += Time.deltaTime;
+            transform.localPosition = Vector3.Lerp(hatchLeft.transform.localPosition, openPositionL, currentMovementTime / totalMovementTime);
+            transform.localPosition = Vector3.Lerp(hatchRight.transform.localPosition, openPositionR, currentMovementTime / totalMovementTime);
+            yield return new WaitForSeconds(1f);
+
+            StartCoroutine(hatchClose());
+        }
+        
+        if (other.CompareTag("Gift"))
+        {
+            Destroy(other);
+        }
     }
-    
-    
+
+    public IEnumerator hatchClose() 
+    {
+        float currentMovementTime = 0f;
+        float totalMovementTime = 1f;
+        
+        while (Vector3.Distance(hatchLeft.transform.localPosition, closedPositionLeft) > 0) 
+        {
+            currentMovementTime += Time.deltaTime;
+            transform.localPosition = Vector3.Lerp(hatchLeft.transform.localPosition, closedPositionLeft, currentMovementTime / totalMovementTime);
+            transform.localPosition = Vector3.Lerp(hatchRight.transform.localPosition, closedPositionRight, currentMovementTime / totalMovementTime);
+            yield return null;
+        }
+    }
 }
